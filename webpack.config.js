ff24855e77327d28c7349d4bb712c2f317e91fd9
Cody Swartz <ctsstc@gmail.com>
2017-11-19 20:22:20 -0800

    Generator init & npm start

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+plugins: [
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
+    Popper: ['popper.js', 'default'],
+  })
+];
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
