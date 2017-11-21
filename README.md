a099aadc8abfa4a6281714024addb3fedb9e41b3
Cody Swartz <ctsstc@gmail.com>
2017-11-21 01:03:15 -0800

    Routes and Views

---
+++ README.md
@@ -0,0 +1,4 @@
+
+### Sites Used
+- Awesome vector creator online - https://vectr.com/cts_ae/b6uk4g8sk
+- https://en.wikipedia.org/wiki/Glider_(Conway%27s_Life)
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    currentPage: 'index',
+    title: 'Express' });
+});
+
+router.get('/rules', (req, res) => {
+  res.render('rules', { currentPage: 'rules' });
+});
+
+router.get('/john-conway', (req, res) => {
+  res.render('john-conway', { currentPage: 'john-conway' });
+});
+
+router.get('/snippet', (req, res) => {
+  res.render('snippet', { currentPage: 'snippet' });
+});
+
+router.get('/resources', (req, res) => {
+  res.render('resources', { currentPage: 'resources' });
 });
 
 module.exports = router;
